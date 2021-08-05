f001aca6d9e829c53cb3a497017f697bcf686203
Hilman F. Rakhmad <hifraofficial@gmail.com>
2021-08-05 13:26:06 +0700

    add comments on routes.js

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -2,27 +2,27 @@ const routes = (handler) => [
   {
     method: 'POST',
     path: '/notes',
-    handler: handler.postNoteHandler,
+    handler: handler.postNoteHandler, // hanya menerima dan menyimpan "satu" note
   },
   {
     method: 'GET',
     path: '/notes',
-    handler: handler.getNotesHandler,
+    handler: handler.getNotesHandler, // mengembalikan "banyak" note
   },
   {
     method: 'GET',
     path: '/notes/{id}',
-    handler: handler.getNoteByIdHandler,
+    handler: handler.getNoteByIdHandler, // mengembalikan "satu" note
   },
   {
     method: 'PUT',
     path: '/notes/{id}',
-    handler: handler.putNoteByIdHandler,
+    handler: handler.putNoteByIdHandler, // hanya menerima dan mengubah "satu" note
   },
   {
     method: 'DELETE',
     path: '/notes/{id}',
-    handler: handler.deleteNoteByIdHandler,
+    handler: handler.deleteNoteByIdHandler, // menghapus "satu" note
   },
 
 ];
