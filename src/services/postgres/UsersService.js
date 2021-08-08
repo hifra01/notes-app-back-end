571d197b89b21afc83bf06921e4af143d391ab91
Hilman F. Rakhmad <hifraofficial@gmail.com>
2021-08-08 22:48:55 +0700

    membuat fungsi getUserById

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -41,6 +41,21 @@ class UsersService {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
     }
   }
+
+  async getUserById(userId) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE id = $1',
+      values: [userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('User tidak ditemukan');
+    }
+
+    return result.rows[0];
+  }
 }
 
 module.exports = UsersService;
